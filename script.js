4dd312d2e5c7c355c88e7b3824683adbdca27d13
Irvine <irvine8roman@gmail.com>
2021-11-06 23:37:21 -0500

    Week 3 Coding Assignment Updates

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
+function firstMinusLast(array) {
+  console.log(array[array.length - 1] - array[0]);
+}
+
+firstMinusLast(ages);
+
+ages.push(8);
+
+firstMinusLast(ages);
+
+const average =
+  ages.reduce((accumulator, currentValue) => accumulator + currentValue) /
+  ages.length;
+
+console.log(ages);
+console.log(average);
