71ac36d077d674370a014447466d7d443bf10498
Irvine <irvine8roman@gmail.com>
2021-11-07 11:42:20 -0600

    Updates to Week 3 Coding Assignment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("#1-------------------"); // 1.
 const ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
 function firstMinusLast(array) {
@@ -16,3 +17,39 @@ const average =
 
 console.log(ages);
 console.log(average);
+
+console.log("#2-------------------"); // 2.
+const names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+
+let sum = 0;
+for (let i = 0; i < names.length; i++) {
+  sum += names[i].length;
+}
+
+let averageNameLength = sum / names.length;
+console.log(averageNameLength);
+
+let listOfNames = "";
+for (let i = 0; i < names.length; i++) {
+  listOfNames += names[i] + " ";
+}
+
+console.log(listOfNames);
+
+console.log("#3-------------------"); // 3.
+
+names[names.length - 1]; //You can access the last element of an array using this piece of code. The .length returns the number of elements in the array
+// while the minus 1 will give you the last element since arrays are zero index
+
+console.log("#4-------------------"); // 4.
+names[0]; //You can access the first element of any array by using the name of the array folowed by [0]. Since arrays are zero index
+
+console.log("#5-------------------"); // 5.
+
+let nameLengths = [];
+
+for (let i = 0; i < names.length; i++) {
+  nameLengths = names.push(names.length[i]);
+}
+
+console.log(nameLengths);
